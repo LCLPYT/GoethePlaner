d20ebe4e534555fa6ab538b54a04fb33ccd98cea
Git-Miso <vs67@protonmail.com>
2020-08-20 19:11:07 +0200

    ScreenNavigator + SplashScreen is now working

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import {
   SafeAreaView,
@@ -8,18 +8,49 @@ import {
   ScrollView,
   View,
   Text,
+  Button,
   StatusBar,
 } from 'react-native';
 
 import SplashScreen from './src/screens/SplashScreen';
 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+
+      <Button
+        title="Zum Stundenplan"
+        onPress={() => navigation.navigate('Stundenplan')}
+      />
+    </View>
+  );
+}
+
+function Stundenplan({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Dein Stundenplan</Text>
+
+      <Button
+        title="Zurück"
+        onPress={() => navigation.navigate('Home')} // or navigation.goBack()
+      />
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
+  
     this.state = { isLoading: true }
   }
 
+  
   wait = async() => {
     return new Promise((resolve) =>
       setTimeout(
@@ -37,23 +68,22 @@ export default class App extends React.Component {
     }
   }
 
-render(){
-  if (this.state.isLoading) {
+  render(){
+    if (this.state.isLoading) {
       return <SplashScreen />;
-  }
+    }
 
     return (
-      <>
       <NavigationContainer>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <Text>Hi :)</Text>
-        </SafeAreaView>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Stundenplan" component={Stundenplan} />
+        </Stack.Navigator>
       </NavigationContainer>
-      </>
-    );
+    )
   };
 }
+
 
 const styles = StyleSheet.create({
 });
