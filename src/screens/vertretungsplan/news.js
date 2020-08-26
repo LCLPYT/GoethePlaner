89b4e92931c7de5e7437a18eaec38bfe2d8db887
LCLP <lukas@lclpnet.work>
2020-08-26 23:11:12 +0200

    added news messages

--- src/screens/vertretungsplan/news.js
+++ src/screens/vertretungsplan/news.js
@@ -1,14 +1,41 @@
 import React from 'react'
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
 
 export default function NewsEntry({ news }) {
-    let newsComponents = news.map(item => <Text key={Math.random().toString()}>{item}</Text>);
+    let newsComponents = [];
+    for (let i = 0; i < news.length; i++) {
+      newsComponents.push(<Text key={Math.random().toString()}>{news[i]}</Text>);
+      if(i < news.length - 1) newsComponents.push(<View style={styles.hr} key={Math.random().toString()} />);
+    }
     return (
-        newsComponents
+      <TouchableWithoutFeedback style={styles.touchable}>
+        <View style={styles.container}>
+          <Text style={styles.headline}>News</Text>
+          <View style={styles.hr}/>
+          {newsComponents}
+        </View>
+      </TouchableWithoutFeedback>
     );
 }
 
 const styles = StyleSheet.create({
-  item: {
-  }
+  touchable: {
+
+  },
+  container: {
+    alignContent: "center",
+    padding: 10,
+    marginHorizontal: 20,
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 20
+  },
+  headline: {
+    fontWeight: 'bold',
+  },
+  hr: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginVertical: 5
+  },
 });
